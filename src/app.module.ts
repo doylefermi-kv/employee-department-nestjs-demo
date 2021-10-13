7caa1315ab384cbb771a5f1173b553682b8fa228
Doyle Fermi <doyle@keyvalue.systems>
2021-10-13 18:27:27 +0530

    Add: Database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { AppGraphQLModule } from './graphql/graphql.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [EmployeesModule, DepartmentsModule, AppGraphQLModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    EmployeesModule,
+    DepartmentsModule,
+    AppGraphQLModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
